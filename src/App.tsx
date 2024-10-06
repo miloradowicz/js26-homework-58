f491723f7bc857a0065ac574567d81d54f338378
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-07 04:28:09 +0600

    testbed created

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Alert from './components/Alert/Alert';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showModal, setShowModal] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        type='button'
+        className='btn btn-primary'
+        onClick={() => {
+          setShowModal(true);
+        }}
+      >
+        Launch demo modal
+      </button>
+      <button
+        type='button'
+        className='btn btn-primary'
+        onClick={() => {
+          setShowAlert(true);
+        }}
+      >
+        Launch demo alert
+      </button>
+      <Modal
+        show={showModal}
+        title='Hello, World!'
+        onClose={() => {
+          setShowModal((show) => !show);
+        }}
+      >
+        hahaha
+      </Modal>
+      <Alert
+        show={showAlert}
+        onClose={() => {
+          setShowAlert((show) => !show);
+        }}
+      >
+        hohoho
+      </Alert>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
